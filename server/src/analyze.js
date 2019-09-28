ebfdc626900f5d2c1bb9e59c0f58e92d5ac512cc
Julia Catalano <jcatalano1990@gmail.com>
2019-09-28 16:15:51 -0400

    feat: google sentiment analysis working in test mode

--- server/src/analyze.js
+++ server/src/analyze.js
@@ -1,6 +1,34 @@
 const router = require('express').Router()
 const axios = require('axios')
 const Title = require('./db/models/storedTitles')
+const language = require('@google-cloud/language')
+require('dotenv').config()
+
+async function quickstart() {
+  // Imports the Google Cloud client library
+
+
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+  // The text to analyze
+  const text = 'Hello, world!';
+
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeSentiment({document: document});
+  const sentiment = result.documentSentiment;
+
+  console.log(`Text: ${text}`);
+  console.log(`Sentiment score: ${sentiment.score}`);
+  console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+}
+
+quickstart()
 
 router.post('/', async (req, res) => {
   try{
