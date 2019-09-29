a32c10a81baf2b09f1a2f5da0616b9a2e726c613
Julia Catalano <jcatalano1990@gmail.com>
2019-09-29 16:20:27 -0400

    feat: loading spinner and clears the page when you want to do a different book

--- client/src/components/Analyze.vue
+++ client/src/components/Analyze.vue
@@ -5,12 +5,18 @@
     <button @click='analyze'>analyze</button>
     <div class='white elevation-2'>
         <h3> results...</h3>
+        <pulse-loader :loading="loading" :color="color" :size="size"></pulse-loader>
         <div>
-          <div>
-          {{this.analysis.score}}
+          <div v-if="this.analysis.score >= .25">
+            Nope! This novel's score is {{this.analysis.score}}, and that sentiment has a magnitude of {{this.analysis.magnitude}}.
           </div>
-          <div>
-          {{this.analysis.magnitude}}
+          <div v-else-if="this.analysis.score <= -.25">
+            Probably - this novel's score is {{this.analysis.score}}, and that sentiment has a magnitude of {{this.analysis.magnitude}}.
+          </div>
+          <div v-else-if="this.analysis.score===null">
+          </div>
+          <div v-else>
+          Maybe! This novel's score is {{this.analysis.score}}, and that sentiment has a magnitude of {{this.analysis.magnitude}}.
           </div>
         </div>
     </div>
@@ -19,20 +25,34 @@
 
 <script>
 import getAnalysis from '@/apiConnection/getAnalysis'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import BounceLoader from 'vue-spinner/src/BounceLoader.vue'
 export default {
   data () {
     return {
       title: '',
-      analysis: {magnitude: null, score: null}
+      analysis: {magnitude: null, score: null},
+      loading: false,
+      color: '#81D8D0',
+      size: '45px'
     }
+  },
+  components: {
+    PulseLoader,
+    BounceLoader
   },
   methods: {
     async analyze () {
+      this.loading = true
+      this.analysis.magnitude = null
+      this.analysis.score = null
       const response = await getAnalysis.analyze({
         title: this.title
       })
+      if (response) {
+        this.loading = false
+      }
       this.analysis = response.data
-      console.log(response.data)
     }
   }
 }
