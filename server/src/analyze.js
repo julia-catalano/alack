5f397b77483c2adc5e8277cf36950de324ddf641
Julia Catalano <jcatalano1990@gmail.com>
2019-10-03 15:18:18 -0400

    cleaning console logs

--- server/src/analyze.js
+++ server/src/analyze.js
@@ -24,23 +24,17 @@ async function quickstart(inputText) {
   const [result] = await client.analyzeSentiment({document: document});
   const sentiment = result.documentSentiment;
   return sentiment
-  console.log(`Text: ${text}`);
-  console.log(`Sentiment score: ${sentiment.score}`);
-  console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
 }
 
 
 router.post('/', async (req, res) => {
 try {
-  console.log('this is the body', req.body.title)
   const data = await Title.findOne({
     where: {
       title: req.body.title
     }
   })
-  console.log(data.author, data.bookUrl, data.title)
   const response = await quickstart(data.text)
-  console.log('response', response)
   const sendThis = {
     title: data.title,
     author: data.author,
@@ -48,7 +42,6 @@ try {
     magnitude: response.magnitude,
     score: response.score
   }
-  console.log(sendThis)
   res.json(sendThis)
   } catch (error) {
     res.status(400).send(`Sorry, I can't read that one right now!`)
